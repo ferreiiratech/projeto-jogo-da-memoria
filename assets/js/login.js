619a1b8fd684b34ca857c2cba330801b54574e38
Leonardo Ferreira <95592421+ferreira-leonardo@users.noreply.github.com>
2023-01-08 01:17:01 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,14 +3,17 @@ const button = document.querySelector('.login-button')
 const form = document.querySelector('.login-form')
 
 const validateInput = ({target}) => {
+    
     if(target.value.length > 2){
         button.removeAttribute('disabled')
         return;
     }
+    
     button.setAttribute('disabled', '')
 }
 
 const handleSubmit = (event) => {
+    
     event.preventDefault();
     
     localStorage.setItem('player', input.value);
